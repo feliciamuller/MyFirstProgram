f968b0a01c617e102f13b4970233a717a50e39a8
felic <felic@FELICIA>
2023-09-06 15:50:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var name = Console.ReadLine();
+            
+        }
+    }
+}
