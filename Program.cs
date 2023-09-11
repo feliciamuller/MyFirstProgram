6d86b273558e0c26e8db8da91caf7a04a1d387ee
Felicia Müller <felicia-muller@hotmail.com>
2023-09-11 14:18:18 +0200

    Lagt till kommentarer

--- Program.cs
+++ Program.cs
@@ -4,9 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Felicia .NET23
+
+
+            int number = 7;
+
+            //checking conditions if the number is higher than 10
+            if (number > 10)
+            {
+                Console.WriteLine("Talet är stort!");
+            }
+           
+            else
+            {
+                Console.WriteLine("Oj. Lågt tal!");
+            }
+
+            //the user enters their name which is saved in the varible name
+            Console.WriteLine("Vad heter du?");
             var name = Console.ReadLine();
-            
+
+            //prints out the users name
+            Console.WriteLine($"Hej {name}");
+
+            //prints out all numbers from 0 to 7
+            for (int i = 0; i <= 7; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
         }
     }
 }
